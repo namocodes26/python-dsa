fa760a71a5b2a91ca2c5d651695fca57d26125c1
Naman Barde <nmnbarde@gmail.com>
2025-08-23 10:56:47 +0530

    initial commit - added another dsa question

---
+++ stock buy sell max one transaction.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumProfit(self, prices):
+        n = len(prices)
+        profit = 0
+        max_profit = 0
+        
+        min_price = prices[0]
+        for i in range(0,n-1):
+            if min_price > prices[i+1]:
+                min_price = prices[i+1]
+            else:
+                profit = prices[i+1]- min_price
+                max_profit = max(profit,max_profit)
+            
+        return max_profit
