352790690d7fae7c4590aad2592897a2993c5a6c
Naman barde <152071710+namocodes69@users.noreply.github.com>
2025-08-23 09:54:16 -0700

    solved kadane's algorithm

---
+++ kadane's algorithm.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSubarraySum(self, arr):
+       n = len(arr)
+       total = 0
+       maxi = arr[0]
+       for i in range(0,n):
+           total+=arr[i]
+           maxi = max(total,maxi)
+           
+           if total < 0:
+               total = 0
+    
+           
+       return maxi
+          
+        
+       
+       
