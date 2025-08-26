354e3b353108f46c3944c8307c5b80990ec47fcc
Naman Barde <nmnbarde@gmail.com>
2025-08-26 18:57:09 +0530

    solved dsa question

--- minimize the height.py
+++ minimize the height.py
@@ -1,36 +1,17 @@
-<<<<<<< HEAD
 class Solution:
     def getMinDiff(self, arr, k):
         n = len(arr)
         arr.sort()
         
-        ans = arr[n-1]-arr[0]
+        ans = arr[-1] - arr[0]   # initial difference
         
         for i in range(n-1):
-            high = max(arr[n-1]-k,arr[i]+k)
-            low = min(arr[0]+k,arr[i+1]-k)
+            high = max(arr[-1] - k, arr[i] + k)
+            low = min(arr[0] + k, arr[i+1] - k)
             
-            if low<0:
+            if low < 0:   # skip if negative
                 continue
-            
-            ans = min(ans,high-low)
+                
+            ans = min(ans, high - low)
         
-=======
-class Solution:
-    def getMinDiff(self, arr, k):
-        n = len(arr)
-        arr.sort()
-        
-        ans = arr[n-1]-arr[0]
-        
-        for i in range(n-1):
-            high = max(arr[n-1]-k,arr[i]+k)
-            low = min(arr[0]+k,arr[i+1]-k)
-            
-            if low<0:
-                continue
-            
-            ans = min(ans,high-low)
-        
->>>>>>> 352790690d7fae7c4590aad2592897a2993c5a6c
         return ans
