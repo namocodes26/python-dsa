8a053177ad26a4514993dfe680abda29e2d6cab4
Naman Barde <nmnbarde@gmail.com>
2025-08-28 15:16:24 +0530

    solved dsa question

--- kadane's algorithm.py
+++ kadane's algorithm.py
@@ -13,24 +13,5 @@ class Solution:
            
        return maxi
           
-        
-       
-
-class Solution:
-    def maxSubarraySum(self, arr):
-       n = len(arr)
-       total = 0
-       maxi = arr[0]
-       for i in range(0,n):
-           total+=arr[i]
-           maxi = max(total,maxi)
-           
-           if total < 0:
-               total = 0
-    
-           
-       return maxi
-          
-        
        
        
