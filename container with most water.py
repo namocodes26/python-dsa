c713ac86d64eabda83e3356b4a8a56fce341a58e
Naman Barde <nmnbarde@gmail.com>
2025-09-04 23:34:36 +0530

    solved dsa question

---
+++ container with most water.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left,right = 0 , len(height)-1
+        max_area = 0
+
+        while left<right:
+            h = min(height[right],height[left])
+            width = right - left
+            max_area = max(max_area,width*h)
+
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right -=1
+        
+        return max_area
